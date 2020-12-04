eeadca6f9ac69ee4984dde47fca20491acf92a4a
Blessing <bntoko@gmail.com>
2020-12-04 17:32:17 +0200

    testing heightMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,6 @@ export class AppComponent implements OnInit{
   });
   public camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
   public controls = new OrbitControls(this.camera, this.renderer.domElement);
-  public geometry = new THREE.Geometry();
   public loader = new THREE.TextureLoader();
   public imgLoader = new THREE.ImageLoader();
 
@@ -32,80 +31,10 @@ export class AppComponent implements OnInit{
     this.controls.target.set(0, 0, 0);
     this.controls.update();
 
+    this.addLight(-1, 2, 4);
+    this.addLight(1, 2, -2);
 
-    this.geometry.vertices.push(
-      new THREE.Vector3(-1, -1, 1), // 0
-      new THREE.Vector3(1, -1, 1), // 1
-      new THREE.Vector3(-1, 1, 1), // 2
-      new THREE.Vector3(1, 1, 1), // 3
-      new THREE.Vector3(-1, -1, -1), // 4
-      new THREE.Vector3(1, -1, -1), // 5
-      new THREE.Vector3(-1, 1, -1), // 6
-      new THREE.Vector3(1, 1, -1), // 7
-    );
-
-    /*
-         6----7
-        /|   /|
-       2----3 |
-       | |  | |
-       | 4--|-5
-       |/   |/
-       0----1
-    */
-
-    this.geometry.faces.push(
-      // front
-      new THREE.Face3(0, 3, 2),
-      new THREE.Face3(0, 1, 3),
-      // right
-      new THREE.Face3(1, 7, 3),
-      new THREE.Face3(1, 5, 7),
-      // back
-      new THREE.Face3(5, 6, 7),
-      new THREE.Face3(5, 4, 6),
-      // left
-      new THREE.Face3(4, 2, 6),
-      new THREE.Face3(4, 0, 2),
-      // top
-      new THREE.Face3(2, 7, 6),
-      new THREE.Face3(2, 3, 7),
-      // bottom
-      new THREE.Face3(4, 1, 0),
-      new THREE.Face3(4, 5, 1),
-    );
-
-    this.geometry.faceVertexUvs[0].push(
-      // front
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 1), new THREE.Vector2(0, 1)],
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 0), new THREE.Vector2(1, 1)],
-      // right
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 1), new THREE.Vector2(0, 1)],
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 0), new THREE.Vector2(1, 1)],
-      // back
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 1), new THREE.Vector2(0, 1)],
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 0), new THREE.Vector2(1, 1)],
-      // left
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 1), new THREE.Vector2(0, 1)],
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 0), new THREE.Vector2(1, 1)],
-      // top
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 1), new THREE.Vector2(0, 1)],
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 0), new THREE.Vector2(1, 1)],
-      // bottom
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 1), new THREE.Vector2(0, 1)],
-      [new THREE.Vector2(0, 0), new THREE.Vector2(1, 0), new THREE.Vector2(1, 1)],
-    );
-
-    // for lighting when using custom geometry
-    this.geometry.computeFaceNormals();
-
-    const texture = this.loader.load('../assets/textures/stars.jpg');
-
-    const cubesArr = [
-      this.makeInst(this.geometry, 0x44ff44, 0, texture),
-      this.makeInst(this.geometry, 0x4444ff, -4, texture),
-      this.makeInst(this.geometry, 0xff4444, 4, texture)
-    ];
+    this.imgLoader.load('../assets/textures/heightMap.png', this.createHeightMap);
 
     window.addEventListener('resize', () => {
       this.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -116,13 +45,6 @@ export class AppComponent implements OnInit{
 
     const animate = (time) => {
       time *= .001;
-
-      cubesArr.forEach((cube, ndx) => {
-        const speed = 1 + ndx * .1;
-        const rotation = time * speed;
-        cube.rotation.x = rotation;
-        cube.rotation.y = rotation;
-      });
 
       this.controls.update();
 
